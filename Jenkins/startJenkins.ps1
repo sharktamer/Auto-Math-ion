ef6f02ee39990afc39df26da8a048a13adb23ae8
mmorissette2 <mmorissette2@cegepthetford.ca>
2023-09-12 13:47:20 -0400

    Ajout d'un docker compose

--- Jenkins/startJenkins.ps1
+++ Jenkins/startJenkins.ps1
@@ -3,4 +3,4 @@ docker pull jenkins/jenkins
 Write-Output("[INFO] Création d'un réseau bridge pour jenkins ...")
 docker network create jenkins
 Write-Output("[INFO] Démarrage du conteneur sur le port 2376 ...")
-docker run --name jenkins-docker --rm --detach --privileged --network jenkins --network-alias docker --env DOCKER_TLS_CERTDIR=/certs --volume jenkins-docker-certs:/certs/client --volume jenkins-data:/var/jenkins_home --publish 2376:2376 docker:dind
+docker compose up .
